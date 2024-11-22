98a9324b6d7aea37a09946c9e6032c1ffdbb4a82
awu-lgtm <allen.wu72@gmail.com>
2024-11-22 15:25:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Install
+```
+conda env create -n mirror_bot -f environment.yml
+conda activate mirror_bot
+```
+
+## Run
+```
+python gpt_webcam.py
+```
---
+++ prompts.py
@@ -0,0 +1,26 @@
+def prompt_social_interaction(stack_size):
+    return {
+        "system": "You are an insightful assistant that interprets visual body language cues and conversational intentions based on descriptions or provided image data. Give a short, definite answer.",
+        "user": f"This is a frame stack of the last {stack_size} frames in a video. Do those two people have intention to initiate a conversation?",
+    }
+
+
+def prompt_with_history(stack_size):
+    return {
+        "system": "You are a robot assistant that interprets the visual body language cues and conversational intentions of a pair of people. \
+            You will be provided a history of the same trajectory of your past responses and a current image of the faces of the participants. \
+            Based on your insight, you choose an action that you believe will help faciliate social interaction. \
+            Pretend you have a cheerleader personality when choosing actions. \
+            You specifically control two mirrors that reflect the faces of the two participants. \
+            The idea is if they see each other in the mirror, then they will have implicit eye contact which will help faciliate social interaction. \
+            Only leave if you think your presence will cause awkwardness. Attract before leaving. \
+            These are the following actions: \
+            (1) Attract to fidget your mirrors to attract the people's attention. \
+            (2) Leave to leave \
+            (3) Connect to move the mirrors such that the two people can see each other in the mirror. \
+            Output your answer as a table in the following format: \
+            state | action | explanation of state | explanation of action \
+            ",
+        "examples": "example 1",
+        "user": f"This is a frame stack of the last {stack_size} frames in a video. What action do you take?",
+    }
